9400f8d19cb1ad1ba60277e09725900c9d6f5137
Heather Christman <efl7a@yahoo.de>
2018-01-07 18:36:17 +0000

    automatically backed up by learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -3,7 +3,7 @@ class Character < ActiveRecord::Base
   belongs_to :actor
 
   def say_that_thing_you_say
-    actor = self.actor
+    actor = Actor.find_or_create_by(self.actor.id)
     "#{actor.full_name} always says: #{self.catchphrase}"
   end
 end
