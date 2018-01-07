fa6e54665a77195b82a22874f15e34c8ba0fd3db
Heather Christman <efl7a@yahoo.de>
2018-01-07 18:30:08 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 7) do
+ActiveRecord::Schema.define(version: 8) do
 
   create_table "actors", force: :cascade do |t|
-    t.string "name"
+    t.string "first_name"
+    t.string "last_name"
   end
 
   create_table "characters", force: :cascade do |t|
