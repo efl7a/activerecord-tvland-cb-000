046072d41cedb297262cf8e2eacd98c211dd3384
Heather Christman <efl7a@yahoo.de>
2018-01-07 18:19:18 +0000

    automatically backed up by learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -2,7 +2,6 @@ class CreateCharacters < ActiveRecord::Migration
   def change
     create_table :characters do |t|
       t.string :name
-      t.string :catchphrase
       t.integer :show_id
       t.integer :actor_id
     end
